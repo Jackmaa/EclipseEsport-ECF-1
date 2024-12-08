bb6da9a917e36462adecda05ed251ce2bfa2801b
Jackmaa <valentingillot@gmail.com>
2024-12-08 08:13:02 +0100

    Final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,14 @@ const menu = document.getElementById("menu");
 const openBtn = document.getElementById("openBtn");
 const closeBtn = document.getElementById("closeBtn");
 
+
 openBtn.onclick = openNav;
-closeBtn.onclick = closeNav;
+document.getElementById('schedule-link').addEventListener("click", () => {
+  closeNav();
+});
+document.getElementById('faq-link').addEventListener("click", () => {
+  closeNav();
+})
 
 function openNav() {
   menu.classList.add("active");
