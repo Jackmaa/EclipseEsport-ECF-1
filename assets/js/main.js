02684e6788f25e7c2832c9272fdab72dd3fabaf4
Jackmaa <valentingillot@gmail.com>
2025-01-02 12:02:30 +0100

    fix du bouton close mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,14 @@ const menu = document.getElementById("menu");
 const openBtn = document.getElementById("openBtn");
 const closeBtn = document.getElementById("closeBtn");
 
-
 openBtn.onclick = openNav;
-document.getElementById('schedule-link').addEventListener("click", () => {
+closeBtn.onclick = closeNav;
+document.getElementById("schedule-link").addEventListener("click", () => {
   closeNav();
 });
-document.getElementById('faq-link').addEventListener("click", () => {
+document.getElementById("faq-link").addEventListener("click", () => {
   closeNav();
-})
+});
 
 function openNav() {
   menu.classList.add("active");
